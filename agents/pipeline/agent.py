ac4d913ced470556a0d862eaa5fbc9265129c8a4
minju00 <bin3994000@naver.com>
2025-11-27 11:00:29 +0900

    feat #23 : ai_search_agent pipeline agent에 연결

--- agents/pipeline/agent.py
+++ agents/pipeline/agent.py
@@ -2,13 +2,14 @@ from google.adk.agents import SequentialAgent
 
 from .sub_agents.query_normalizer_agent.agent import query_agent
 from .sub_agents.title_router.agent import router_agent
-
+from .sub_agents.ai_search_agent.agent import root_agent as ai_search_agent
 pipeline_agent = SequentialAgent(
     name="pipeline_agent",
-    description="사용자 질문을 정규화 → 라우팅 생성까지 순차 처리하는 파이프라인 에이전트",
+    description="사용자 질문을 정규화 → 라우팅 생성, ai search를 이용한 답변 생성까지 순차 처리하는 파이프라인 에이전트",
     sub_agents=[
         query_agent,
         router_agent,
+        ai_search_agent,
     ]
 )
 
